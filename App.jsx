ef98aa412480e3acaa4bb8783f064119fdb82b01
Samir Jabre <samirjabre8@gmail.com>
2025-07-04 21:05:39 +0300

    feat: add redux-persist for state persistence and integrate AsyncStorage

--- App.jsx
+++ App.jsx
@@ -2,13 +2,16 @@ import { StatusBar } from 'react-native'
 import React from 'react'
 import "./global.css"
 import { Provider } from 'react-redux'
-import { store } from './src/Redux/store'
+import { store, persistor } from './src/Redux/store'
 import MainNavigation from './src/MainNavigation'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <MainNavigation />
+      <PersistGate loading={null} persistor={persistor}>
+        <MainNavigation />
+      </PersistGate>
     </Provider>
   )
 }
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './Slices/productSlice';
 import cartReducer from './Slices/cartSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage,
+}
 
 export const store = configureStore({
     reducer: {
-        products: productReducer,
-        cart: cartReducer
+        products: persistReducer(persistConfig, productReducer),
+        cart: persistReducer(persistConfig, cartReducer),
     },
 });
+
+export const persistor = persistStore(store);
